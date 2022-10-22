36c34496e51baacfc15f183727c61444161a29da
WilsonYG114 <86198304+WilsonYG114@users.noreply.github.com>
2022-10-22 01:41:01 -0400

    Add files via upload

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -36,25 +36,68 @@ class FeedViewController: UIViewController, UITableViewDelegate,UITableViewDataS
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let post = posts[section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        return  comments.count + 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell")as! PostCell
+        
+        let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        
+        if indexPath.row == 0{
+            
+            let user = post["author"] as! PFUser
+            cell.usernameLabel.text = user.username
+            cell.commentLabel.text = post["caption"] as! String
+            
+            let imageFile = post["image"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)!
+            
+            cell.photoview.af_setImage(withURL: url)
+            return cell
+        }else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let post = posts[indexPath.row]
         
-        let user = post["author"] as! PFUser
-        cell.usernameLabel.text = user.username
-        cell.commentLabel.text = post["caption"] as! String
+        let comment = PFObject(className: "Comments")
+        comment["text"] = "This is a random comment"
+        comment["post"] = post
+        comment["author"] = PFUser.current()!
         
-        let imageFile = post["image"] as! PFFileObject
-        let urlString = imageFile.url!
-        let url = URL(string: urlString)!
         
-        cell.photoview.af_setImage(withURL: url)
-        return cell
+        post.add(comment, forKey: "comments")
+        
+        post.saveInBackground{(success, error) in
+            if success {
+                print("Comment saved")
+            } else{
+                print("Error saving comment")
+            }
+        
+        }
     }
 
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        guard let windowsScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowsScene.delegate as? SceneDelegate else {return}
+        delegate.window?.rootViewController = loginViewController
+    }
+    
     /*
     // MARK: - Navigation
 
